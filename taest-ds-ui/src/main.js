ae8ef7f8426cd384da3a7cf09a4c3ba200b1c395
taest <htaest@foxmail.com>
2025-01-06 12:27:07 +0800

    init(前端页面)  引入Tailwind css

--- taest-ds-ui/src/main.js
+++ taest-ds-ui/src/main.js
@@ -2,10 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import router from './router/router.js';
 import {createPinia} from 'pinia'
+import './tailwindcss.css'
 const app = createApp(App)
-const pinia = createPinia();
+const pinia = createPinia()
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(ElementPlus)
 app.use(router).use(pinia)
 app.mount('#app')
