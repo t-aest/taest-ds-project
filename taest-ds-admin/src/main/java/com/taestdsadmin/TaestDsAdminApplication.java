393444432e53e68408fcee86416103dcdc29fa01
taest <htaest@foxmail.com>
2025-01-09 17:53:39 +0800

    Initialize the backend project

---
+++ taest-ds-admin/src/main/java/com/taestdsadmin/TaestDsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.taestdsadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaestDsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaestDsAdminApplication.class, args);
+    }
+
+}
